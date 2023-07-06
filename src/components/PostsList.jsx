36e9095632933dd61e761b46a4de69ee651c0fb8
Ramadan Bozaliev <ramco95@abv.bg>
2023-07-06 14:34:58 +0300

    outputting list data

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -8,9 +8,7 @@ function PostsList({ isPosting, onStopPosting }) {
   const [posts, setPosts] = useState([]);
 
   function addPostHandler(postData) {
-    setPosts((existingPosts) => {
-      [postData, ...existingPosts];
-    });
+    setPosts((existingPosts) => [postData, ...existingPosts]);
   }
   return (
     <>
@@ -19,9 +17,21 @@ function PostsList({ isPosting, onStopPosting }) {
           <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
-      <ul className={classes.posts}>
-        <Post author='Manuel' body='Check out the full course!' />
-      </ul>
+      {posts.length > 0 && (
+        <ul className={classes.posts}>
+          {posts.map((post) => {
+            return (
+              <Post key={post.body} author={post.author} body={post.body} />
+            );
+          })}
+        </ul>
+      )}
+      {posts.length === 0 && (
+        <div style={{ textAlign: 'center', color: 'white' }}>
+          <h2>There are no posts yet.</h2>
+          <p>Start adding some!</p>
+        </div>
+      )}
     </>
   );
 }
